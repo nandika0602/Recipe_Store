5b47113406c9b8df58003f8920f3090c28fbfa81
nandika0602 <nandika.d0602@gmail.com>
2024-04-15 11:46:28 +0530

    update

--- src/Cart.js
+++ src/Cart.js
@@ -14,52 +14,139 @@ const Cart = ({ cart, removeFromCart }) => {
   // console.log(cart);
   return (
     <div>
-      <Typography sx={{fontSize: '30px', fontWeight: 'bolder', textAlign: 'center', padding: '20px', color: 'gray'}}>{cart.length ? 'CART ITEMS' : 'NO ITEMS ADDED TO CART'}</Typography>
+      <Typography
+        sx={{
+          fontSize: "30px",
+          fontWeight: "bolder",
+          textAlign: "center",
+          padding: "20px",
+          color: "gray",
+          marginTop: "100px",
+        }}
+      >
+        {cart.length ? "CART ITEMS" : "NO ITEMS ADDED TO CART"}
+      </Typography>
       {/* <Link to="/">
         <Button variant="contained">Go Back To Listing Page</Button>
       </Link> */}
       <Stack direction={"row"} className="cart_card">
-        {cart?.map((item) => {
-          return (
-            <Card
-              key={item.id}
-              sx={{
-                height: '340px',
-                marginLeft: "20px",
-                width: '15%',
-                marginBottom: '30px',
-                position: 'relative '
-              }}
-            >
-              <CardContent>
-                <CardMedia
-                  component="img"
-                  height="200px"
-                  width="20px"
-                  image={item?.image}
-                ></CardMedia>
-                <Typography sx={{
-                  padding: '8px',
-                  fontWeight: 'bold',
-                  color: '#00538C'
-                }}>{item?.name.toUpperCase()}</Typography>
+        {cart.length ? (
+          cart?.map((item) => {
+            return (
+              <Card
+                key={item.id}
+                sx={{
+                  height: "340px",
+                  marginLeft: "20px",
+                  width: "15%",
+                  marginBottom: "30px",
+                  position: "relative ",
+                  // padding: "20px",
+                  // paddingY: "40px",
+                }}
+              >
+                <Link to={`/dashboard/${item.id}`}>
+                  {/* <CardMedia
+                    component="img"
+                    height="300"
+                    width="100px"
+                    // image={list.image}
+                    alt="Paella dish"
+                    // onClick={() => getDetail(list.id)}
+                  /> */}
+                </Link>
+                <CardContent>
+                  <Link
+                    to={`/dashboard/${item.id}`}
+                    state={{
+                      detail: item,
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      height="200px"
+                      width="20px"
+                      image={item?.image}
+                    ></CardMedia>
+                    {/* {item.id} */}
+                  </Link>
+                  <Typography
+                    sx={{
+                      padding: "8px",
+                      fontWeight: "bold",
+                      color: "#00538C",
+                    }}
+                  >
+                    {item?.name.toUpperCase()}
+                  </Typography>
+                </CardContent>
                 <Button
                   color="error"
                   variant="contained"
                   onClick={() => removeFromCart(item?.id)}
                   sx={{
-                    marginLeft: '50px',
-                    marginTop: '5px',
-                    position: 'absolute',
-                    top: '82%'
+                    // marginLeft: "25%",
+                    // marginTop: "15px",
+                    position: "absolute",
+                    top: "89%",
+                    width: "100%",
                   }}
                 >
                   REMOVE
                 </Button>
-              </CardContent>
-            </Card>
-          );
-        })}
+              </Card>
+            );
+          })
+        ) : (
+          <Typography
+            sx={{
+              padding: "8px",
+              fontWeight: "bold",
+              color: "#00538C",
+            }}
+          ></Typography>
+        )}
+        {/* // {cart?.map((item) => {
+        //   return (
+        //     <Card
+        //       key={item.id}
+        //       sx={{
+        //         height: '340px',
+        //         marginLeft: "20px",
+        //         width: '15%',
+        //         marginBottom: '30px',
+        //         position: 'relative '
+        //       }}
+        //     >
+        //       <CardContent>
+        //         <CardMedia
+        //           component="img"
+        //           height="200px"
+        //           width="20px"
+        //           image={item?.image}
+        //         ></CardMedia>
+        //         <Typography sx={{
+        //           padding: '8px',
+        //           fontWeight: 'bold',
+        //           color: '#00538C'
+        //         }}>{item?.name.toUpperCase()}</Typography>
+        //         <Button
+        //           color="error"
+        //           variant="contained"
+        //           onClick={() => removeFromCart(item?.id)}
+        //           sx={{
+        //             marginLeft: '50px',
+        //             marginTop: '5px',
+        //             position: 'absolute',
+        //             top: '82%'
+        //           }}
+        //         >
+        //           REMOVE
+        //         </Button>
+        //       </CardContent>
+        //     </Card>
+        //   );
+        // })} */}
       </Stack>
     </div>
   );
--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -5,61 +5,102 @@ import {
   CardMedia,
   Button,
 } from "@mui/material";
+import { useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const ItemDetail = ({ detail }) => {
+  const [show, setShow] = useState(false);
+  const pathname = useLocation();
+  const detail1 = pathname?.state?.detail;
+
+  const toggleCard = (type) => {
+    if (type === "ing") {
+      setShow(type);
+    }
+    if (type === "ins") {
+      setShow(type);
+    }
+  };
+  if (detail1) detail = detail1;
   return (
-    <div className="item_details">
-      <Card>
-        <CardMedia
-          component="img"
-          height="300"
-          width="100px"
-          image={detail.image}
-          alt="Paella dish"
-        />
-        <CardContent key={detail.id}>
-          <Typography sx={{ fontWeight: "bold", color: "#005A9C" }}>
-            NAME: {detail.name}
-          </Typography>
-          <Typography sx={{ fontWeight: "bold", color: "#4B9CD3" }}>
-            CUISINE: {detail.cuisine}
-          </Typography>
-          <Typography sx={{ fontWeight: "bold", color: "#4B9CD3" }}>
-            TAGS: {detail?.tags?.join(", ")}
-          </Typography>
-          <Typography
-            sx={{ color: "green", fontSize: "20px", fontWeight: "bold" }}
+    <>
+      <div className="item_details">
+        <Card
+          sx={{
+            height: "500px",
+          }}
+        >
+          <CardMedia
+            component="img"
+            height="300px"
+            width="100px"
+            image={detail.image}
+            alt="Paella dish"
+            sx={{
+              objectFit: "contain",
+            }}
+          />
+          <CardContent key={detail.id}>
+            <Typography sx={{ fontWeight: "bold", color: "#005A9C" }}>
+              NAME: {detail.name}
+            </Typography>
+            <Typography sx={{ fontWeight: "bold", color: "#4B9CD3" }}>
+              CUISINE: {detail.cuisine}
+            </Typography>
+            <Typography sx={{ fontWeight: "bold", color: "#4B9CD3" }}>
+              TAGS: {detail?.tags?.join(", ")}
+            </Typography>
+            <Typography
+              sx={{ color: "green", fontSize: "20px", fontWeight: "bold" }}
+            >
+              RATINGn: {detail.rating}
+            </Typography>
+            <Link to="/list">
+              <Button variant="contained">Go Back To Listing Page</Button>
+            </Link>
+          </CardContent>
+        </Card>
+        <div>
+          <Card
+            sx={{ padding: "20px", marginBottom: "30px" }}
+            onClick={() => toggleCard("ing")}
           >
-            RATING: {detail.rating}
-          </Typography>
-          <Link to="/list">
-          <Button variant="contained">Go Back To Listing Page</Button>
-          </Link>
-        </CardContent>
-      </Card>
-      <Card sx={{ padding: "20px" }}>
-        <h1>{"ingredients".toUpperCase()}</h1>
-        {detail?.ingredients?.map((ing, i) => {
-          return (
-            <Typography key={i}>
-              {i + 1}. {ing}
-            </Typography>
-          );
-        })}
-      </Card>
-      <Card sx={{ padding: "20px" }}>
-        <h1>{"instructions".toUpperCase()}</h1>
-        {detail?.instructions?.map((e, i) => {
-          return (
-            <Typography key={i}>
-              {i + 1}. {e}
-            </Typography>
-          );
-        })}
-      </Card>
-    </div>
+            <h1
+              style={{
+                cursor: "pointer",
+              }}
+            >
+              {"ingredients".toUpperCase()}
+            </h1>
+            {detail?.ingredients?.map((ing, i) => {
+              return (
+                show === "ing" && (
+                  <Typography key={i}>
+                    {i + 1}. {ing}{" "}
+                  </Typography>
+                )
+              );
+            })}
+          </Card>
+          <Card
+            sx={{ padding: "20px", cursor: "pointer" }}
+            onClick={() => toggleCard("ins")}
+          >
+            <h1>{"instructions".toUpperCase()}</h1>
+            {detail?.instructions?.map((e, i) => {
+              return (
+                show === "ins" && (
+                  <Typography key={i}>
+                    {i + 1}. {e}
+                  </Typography>
+                )
+              );
+            })}
+          </Card>
+        </div>
+      </div>
+    </>
   );
 };
 
